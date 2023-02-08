613ded7d28db9d03e6996941a51db604ad87cf8d
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-08 07:49:56 +0530

    Add : Program.cs program

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿namespace HelloWorld
+{
+    class Program //class name can writte nanything
+    {
+        public static void Main(string[] args) 
+        {
+            //when class next class means next file file class like this.....................
+
+            //Username.name();
+            // we canm also access next file value like this .....................................
+            //Console.WriteLine(Username.Stri);
+
+            //FlipCoin.CalculatePercentage();
+
+
+            //power2.CalaculateNum();
+
+            //LeapYear.LYearorNot();
+
+            PrimeCheck.CheckPrime();
+
+            //Harmonic.Harmonicfind();
+
+
+
+            //.................................................................................................................
+
+            /*int num1 = 7;
+            int num2 = 5;
+            int sum = num1 + num2;*/
+
+
+
+            // Console.WriteLine("Krishna Kumar Welcome to .Net Framwork ");
+            //Console.WriteLine(sum);
+
+            //DATA TYPE...........................
+
+            /*int num = 7;
+            double num1 = 10.9;
+            bool val = true;
+            char singleChar = 's';
+            string name = "krishnakumar";
+            Console.WriteLine("Integer" + num);
+            Console.WriteLine("double" + num1);
+            Console.WriteLine("bool" + val);
+            Console.WriteLine("singleChar " + singleChar);
+            Console.WriteLine("String" +name);
+
+            int nums = 100;
+            Console.WriteLine(nums);
+            string str = "Krishna kumar";
+            Console.WriteLine(str + nums);
+            Console.WriteLine("This is {0} number !!!");
+
+            //when take input from users!!! ............................
+
+
+            string takeInput = Console.ReadLine();
+            Console.WriteLine("User input :-" + takeInput);
+            Console.WriteLine("The name is  {0} {1} {2} {3} ", takeInput, num, num1, val);*/
+
+            /*string name1 = Console.ReadLine();
+            Console.WriteLine("Hello " +name1+ ",How are you!!!");
+            Console.WriteLine("Hello {0} ,How are you " ,name1);
+
+
+            // it help to access value ...........................
+            int nums = 100;
+            Console.WriteLine("This is {0} number !!!" ,nums);*/
+
+            /*string nm = "Krishna kumar";
+            Console.WriteLine(nm.Length);
+            Console.WriteLine(nm);
+
+            string name = Console.ReadLine();
+
+            if (name.Length > 3) 
+            {
+                Console.WriteLine("Hello {0} ,How are you ", name);
+            
+            }
+            else
+            {
+                Console.WriteLine("The name is not having minumum 3 char!!");
+
+            }
+            */
+
+            // Two number add through enter user
+            //NOTE:- When take input always string firstly convert!!! int, bool, ect...
+            /* Console.WriteLine("Enter a first Number");
+             int num1= Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter a Second Number");
+             int num2= Convert.ToInt32(Console.ReadLine());
+
+             int  num3=num1 + num2;
+             Console.WriteLine("Two number add " + num3);*/
+
+
+            /*string namespa = Console.ReadLine();
+
+            if (namespa.Length > 3 && namespa.Length < 5)
+            {
+                Console.WriteLine("Hello {0} ,How are you ", namespa);
+
+            }
+            else if (namespa.Length > 5)
+            {
+                Console.WriteLine("Your are enter char grater than 5");
+
+            }
+            else {
+                Console.WriteLine("The name is not having minumum 3 char!!");
+                    
+                    
+            }*/
+
+        }
+
+
+    }
+}
