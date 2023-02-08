d677938dc88c485813190f42eb09b1b5706ecd7f
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-08 07:53:03 +0530

    Add : Username.cs program

---
+++ Username.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Username
+    {
+        public static string Stri = "Krishna";
+        public static void name()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            if (name.Length > 3)
+            {
+                Console.WriteLine("Hello {0} ,How are you ", name);
+
+            }
+            else
+            {
+                Console.WriteLine("The name is not having minumum 3 char!!");
+
+            }
+        }
+
+    }
+}
